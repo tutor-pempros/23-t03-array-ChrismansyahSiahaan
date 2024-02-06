06b5afdd7aa42762b02f36fc07b1e138bcda7b0b
ChrismansyahSiahaan <chrismansyahsiahaan19@gmail.com>
2024-02-06 21:40:34 +0700

    12S23003

--- t03_03.c
+++ t03_03.c
@@ -1,42 +1,43 @@
 // 12S23003 - Chrismansyah T Siahaan
-
 #include <stdio.h>
 
 int main() {
-  int n;
+  int n, i, min, max, sum;
+  float avg;
+
   scanf("%d", &n);
 
-  int arr[n];
-  for (int i = 0; i < n; i++) {
-    scanf("%d", &arr[i]);
+  min = 101;
+  max = -101;
+  sum = 0;
+
+  for (i = 0; i < n; i++) {
+    int num;
+    scanf("%d", &num);
+
+    if (num < min) {
+      min = num;
+    } else if (num > max) {
+      max = num;
+    }
+
+    sum += num;
   }
 
-  int min = arr[0], max = arr[0], sum = 0, max_diff = 0;
-  for (int i = 0; i < n; i++) {
-    if (arr[i] < min) {
-      min = arr[i];
-    }
+  avg = (float)sum / n;
 
-    if (arr[i] > max) {
-      max = arr[i];
-    }
-
-    sum += arr[i];
-
-    if (i > 0) {
-      int diff = abs(arr[i] - arr[i - 1]);
-      if (diff > max_diff) {
-        max_diff = diff;
-      }
+  int max_diff = -1;
+  for (i = 0; i < n - 1; i++) {
+    int diff = abs(n[i] - n[i + 1]);
+    if (diff > max_diff) {
+      max_diff = diff;
     }
   }
 
-  float average = (float)sum / n;
-
-  printf("Nilai terkecil: %d\n", min);
-  printf("Nilai terbesar: %d\n", max);
-  printf("Nilai rata-rata: %.2f\n", average);
-  printf("Jarak nilai terjauh: %d\n", max_diff);
+  printf("%d\n", min);
+  printf("%d\n", max);
+  printf("%.2f\n", avg);
+  printf("%d\n", max_diff);
 
   return 0;
 }
